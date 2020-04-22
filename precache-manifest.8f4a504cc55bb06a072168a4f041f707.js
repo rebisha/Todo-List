929fa25f9f5e6df6164c7860dab5b21c255f9db5
Rebisha Pokharel <rebisha.pokharel@isobar.com>
2020-04-22 15:45:18 +1000

    Updates

---
+++ precache-manifest.8f4a504cc55bb06a072168a4f041f707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b50f9a8bb895ca50508fdd6547c14f",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "d6edb46ee9a2db3efd4b",
+    "url": "/Todo-List/static/css/main.157bde07.chunk.css"
+  },
+  {
+    "revision": "c38418995462c7fb51a0",
+    "url": "/Todo-List/static/js/2.dd365bbb.chunk.js"
+  },
+  {
+    "revision": "b00e8fb2529ae472d74f22278c51db0c",
+    "url": "/Todo-List/static/js/2.dd365bbb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6edb46ee9a2db3efd4b",
+    "url": "/Todo-List/static/js/main.0102bd55.chunk.js"
+  },
+  {
+    "revision": "59e346ee62e6b651920e",
+    "url": "/Todo-List/static/js/runtime-main.9604111c.js"
+  }
+]);
